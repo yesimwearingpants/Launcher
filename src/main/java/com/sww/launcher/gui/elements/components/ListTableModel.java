c9f767d7e202e85302126eda974d5f3365c64858
Julien Kyou <yesimwearingpants@gmail.com>
2015-06-14 18:56:37 -0400

    Refined ListTableModel Correctly

--- src/main/java/com/sww/launcher/gui/elements/components/ListTableModel.java
+++ src/main/java/com/sww/launcher/gui/elements/components/ListTableModel.java
@@ -51,111 +51,11 @@ public class ListTableModel extends AbstractTableModel implements Serializable {
     public void setDataVector(List<Profile> list, Object[] columnNames) {
         this.dataVector = nonNullVector(list);
         this.columnIdentifiers = nonNullArray(columnNames);
-        justifyRows(0, getRowCount());
         fireTableStructureChanged();
     }
 
     public void newDataAvailable(TableModelEvent event) {
         fireTableChanged(event);
-    }
-
-    private void justifyRows(int from, int to) {
-
-        for (int i = from; i < to; i++) {
-            if (dataVector.get(i) == null) {
-                dataVector.set(i, new ArrayList<>());
-            }
-            dataVector.get(i);
-        }
-    }
-
-    public void setNumRows(int rowCount) {
-        int old = getRowCount();
-        if (old == rowCount) {
-            return;
-        }
-        if (rowCount <= old) {
-            fireTableRowsDeleted(rowCount, old-1);
-        }
-        else {
-            justifyRows(old, rowCount);
-            fireTableRowsInserted(old, rowCount-1);
-        }
-    }
-
-    public void setRowCount(int rowCount) {
-        setNumRows(rowCount);
-    }
-
-    private static int gcd(int i, int j) {
-        return (j == 0) ? i : gcd(j, i%j);
-    }
-
-    private static List rotate(List v, int a, int b, int shift) {
-        int size = b - a;
-        int r = size - shift;
-        int g = gcd(size, r);
-        for(int i = 0; i < g; i++) {
-            int to = i;
-            Object tmp = v.get(a + to);
-            for(int from = (to + r) % size; from != i; from = (to + r) % size) {
-                v.set((a + to), v.get(a + from));
-                to = from;
-            }
-            v.set(a + to, tmp);
-        }
-        return v;
-    }
-
-    public void moveRow(int start, int end, int to) {
-        int shift = to - start;
-        int first, last;
-        if (shift < 0) {
-            first = to;
-            last = end;
-        }
-        else {
-            first = start;
-            last = to + end - start;
-        }
-        rotate(dataVector, first, last + 1, shift);
-
-        fireTableRowsUpdated(first, last);
-    }
-
-    public void removeRow(int row) {
-        dataVector.remove(row);
-        fireTableRowsDeleted(row, row);
-    }
-
-    public void setColumnIdentifiers(Object[] columnIdentifiers) {
-        setDataVector(dataVector, columnIdentifiers);
-    }
-
-    public void setColumnCount(int columnCount) {
-        justifyRows(0, getRowCount());
-        fireTableStructureChanged();
-    }
-
-    public void addColumn(Object columnName) {
-        addColumn(columnName, (List)null);
-    }
-
-    public void addColumn(Object columnName, List columnData) {
-        if (columnData != null) {
-            int columnSize = columnData.size();
-            justifyRows(0, getRowCount());
-            int newColumn = getColumnCount() - 1;
-            for(int i = 0; i < columnSize; i++) {
-                  List row = (List)dataVector.get(i);
-                  row.set(newColumn, columnData.get(i));
-            }
-        }
-        else {
-            justifyRows(0, getRowCount());
-        }
-
-        fireTableStructureChanged();
     }
 
     public int getRowCount() {
@@ -168,8 +68,6 @@ public class ListTableModel extends AbstractTableModel implements Serializable {
 
     public String getColumnName(int column) {
         Object id = null;
-        // This test is to cover the case when
-        // getColumnCount has been subclassed by mistake ...
         if (column < columnIdentifiers.length && (column >= 0)) {
             id = columnIdentifiers[column];
         }
@@ -184,12 +82,6 @@ public class ListTableModel extends AbstractTableModel implements Serializable {
     public Object getValueAt(int row, int column) {
         Profile rowVector = (Profile) dataVector.get(row);
         return rowVector.get(column);
-    }
-
-    public void setValueAt(Object aValue, int row, int column) {
-        List rowVector = (List)dataVector.get(row);
-        rowVector.set(column, aValue);
-        fireTableCellUpdated(row, column);
     }
 
 }
